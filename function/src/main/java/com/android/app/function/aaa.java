dcf26c10eb65c719e64f1e8b6442af6e936e74ab
frodoking <awangyun8@gmail.com>
2014-12-26 11:56:13 +0800

    init project

---
+++ function/src/main/java/com/android/app/function/aaa.java
@@ -0,0 +1,7 @@
+package com.android.app.function;
+
+/**
+ * Created by xuwei19 on 2014/12/26.
+ */
+public class aaa {
+}
---
+++ function/src/main/java/com/android/app/test/test.java
@@ -0,0 +1,7 @@
+package com.android.app.test;
+
+/**
+ * Created by xuwei19 on 2014/12/26.
+ */
+public class test {
+}
