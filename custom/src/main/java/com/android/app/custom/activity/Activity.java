6226971ed6347919216cdf1dcc0b69c57ba620d9
frodoking <awangyun8@gmail.com>
2015-01-27 16:20:49 +0800

    添加framework的依赖

--- custom/src/main/java/com/android/app/custom/activity/Activity.java
+++
@@ -1,7 +0,0 @@
-package com.android.app.custom.activity;
-
-/**
- * Created by xuwei19 on 2014/12/26.
- */
-public class Activity {
-}
--- function/src/main/java/com/android/app/test/test.java
+++
@@ -1,7 +0,0 @@
-package com.android.app.test;
-
-/**
- * Created by xuwei19 on 2014/12/26.
- */
-public class test {
-}
