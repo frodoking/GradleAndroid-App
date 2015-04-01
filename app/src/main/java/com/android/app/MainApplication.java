3a65547d45d3533111dfc0a19f55a0fa3f164561
frodoking <awangyun8@gmail.com>
2015-04-01 19:01:59 +0800

    代码引用整理

--- app/src/main/java/com/android/app/MainApplication.java
+++ app/src/main/java/com/android/app/MainApplication.java
@@ -1,16 +1,21 @@
 package com.android.app;
 
-import android.app.Application;
 import android.content.Context;
+import com.android.app.framework.config.Configuration;
 
 import com.android.app.function.dynamicloading.DynamicLoadingUtil;
 
 /**
  * Created by frodo on 2015/2/4.
  */
-public class MainApplication extends Application {
+public class MainApplication extends AppApplication {
     @Override
     protected void attachBaseContext(Context base) {
         DynamicLoadingUtil.attachBaseContext(base);
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return null;
     }
 }
